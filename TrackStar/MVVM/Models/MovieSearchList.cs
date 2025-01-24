9b6fd1328e70e01d01c204f1b38c24851f5d0b84
yagizerdem <2yagiz004@gmail.com>
2025-01-24 16:12:57 +0300

    update

--- TrackStar/MVVM/Models/MovieSearchList.cs
+++ TrackStar/MVVM/Models/MovieSearchList.cs
@@ -11,17 +11,26 @@ namespace TrackStar.MVVM.Models
     public partial class MovieSearchList
     {
         public List<Search> Search { get; set; }
+        [JsonProperty("totalResults")]
         public string totalResults { get; set; }
+        [JsonProperty("Response")]
         public string Response { get; set; }
 
     }
 
     public class Search
     {
+        [JsonProperty("Title")]
         public string Title { get; set; }
-        public string Year { get; set; }
+
+        [JsonProperty("Year")]
+        [JsonConverter(typeof(ParseStringConverter))]
+        public long Year { get; set; }
+        [JsonProperty("imdbID")]
         public string imdbID { get; set; }
+        [JsonProperty("Type")]
         public string Type { get; set; }
+        [JsonProperty("Poster")]
         public string Poster { get; set; }
     }
 
