9c2274db1331f6c9d9ab41ad4dcaf769add56e65
yagizerdem <yagizerdem819@gmail.com>
2025-08-12 03:34:29 +0300

    navigation logic added

---
+++ TrackStar/ViewModels/HomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackStar.ViewModels
+{
+    public class HomeViewModel : ViewModelBase
+    {
+        public HomeViewModel()
+        {
+            
+        }
+    }
+}
--- TrackStar/ViewModels/MainViewModel.cs
+++ TrackStar/ViewModels/MainViewModel.cs
@@ -1,13 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using TrackStar.Commands;
 
 namespace TrackStar.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
-        public MainViewModel() { }
+
+        public RelayCommand<object> NavigateHome { get; set; }
+
+        public RelayCommand<object> NavigateSearch { get; set; }
+
+        public RelayCommand<object> NavigateSaved { get; set; }
+
+        
+        private ViewModelBase _currentViewModel;
+        public ViewModelBase CurrentViewModel
+        {
+            get { return _currentViewModel; }
+            set
+            {
+                if (_currentViewModel != value)
+                {
+                    SetProperty(ref _currentViewModel, value);
+                }
+            }
+        }
+
+        private ViewModelBase _homeViewModel;
+
+        private ViewModelBase _searchViewModel;
+
+        private ViewModelBase _savedViewModel;
+        public MainViewModel() {
+
+            _homeViewModel = new HomeViewModel();
+            _searchViewModel = new SearchViewModel();
+
+
+            NavigateHome = new RelayCommand<object>(o => NavigateHomeExecute(), _ => true);
+            NavigateSearch = new RelayCommand<object>(o => NavigateSearchExecute(), _ => true);
+
+            CurrentViewModel = _homeViewModel;
+        }
+
+
+        public void NavigateHomeExecute() => CurrentViewModel = _homeViewModel;
+        public void NavigateSearchExecute() => CurrentViewModel = _searchViewModel;
+    
+
     }
 }
